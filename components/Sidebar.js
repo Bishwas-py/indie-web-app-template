382a6d9799dac1984b5d550097caf106c0bd9f4e
bishwas-py <bishwasbh@gmail.com>
2022-04-22 14:57:30 +0545

    Something New

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -9,21 +9,25 @@ export default function Sidebar() {
     const [open, setOpen] = useState(false);
     return (
         <div
-            className={`${open ? "w-64 sm:relative ultra-sm:fixed ultra-sm:w-10/12" : "w-20"} relative align-middle duration-700 transition-all h-screen p-3 pt-8 bg-dark-purple`}>
+            className={`${open ? "w-64 sm:relative ultra-sm:fixed ultra-sm:w-full" : "w-20"} relative align-middle duration-700 transition-all h-screen p-3 pt-8 bg-dark-purple`}>
             <div onClick={() => setOpen(!open)}
-                 className="absolute w-7 h-7 cursor-pointer -right-3 top-9
+                 className={`absolute w-7 h-7 cursor-pointer -right-3 top-9
                         flex justify-center align-middle place-items-center
-                        border-2 border-dark-purple rounded-full bg-white">
+                        duration-500
+                        ${open && `ultra-sm:-right-0`}
+                        border-2 border-dark-purple rounded-full bg-white`}>
                 <FontAwesomeIcon icon={faChevronLeft}
                                  className={`${!open && "rotate-180"} duration-700`}/>
             </div>
-            <div className={`flex ${!open ? "p-2 w-12 self-center mx-auto" : "py-2 px-3 gap-x-4"} items-center` +
-                ` cursor-pointer rounded-md bg-light-white hover:bg-blue-500`}>
-                <img src={"/favicon.svg"} alt=''
-                     className={`cursor-pointer duration-500 ` +
-                         `${open ? "w-6" : "w-auto rotate-[360deg]"}`}/>
-                <h1 className={`text-white origin-left font-medium 
+            <div className={"w-full pl-1"}>
+                <div className={`flex ${!open ? "p-2 w-12" : "py-2 px-3 gap-x-4"} items-center` +
+                    ` cursor-pointer rounded-md bg-light-white hover:bg-blue-500`}>
+                    <img src={"/favicon.svg"} alt=''
+                         className={`cursor-pointer duration-500 ` +
+                             `${open ? "w-6" : "w-auto rotate-[360deg]"}`}/>
+                    <h1 className={`text-white origin-left font-medium 
                             duration-300 ${!open && "scale-0 h-0"}`}>Webmatrices Apps</h1>
+                </div>
             </div>
             <div className={"pt-3 overflow-y-scroll h-[89%] scrollbar-hide"}>
                 {paths.map((path, index) => (
